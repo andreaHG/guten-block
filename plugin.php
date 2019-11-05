4e51f0763b667b8df90f407e6b0ea6cdf4b0b4a0
andreaHG <andrea.hrnndzg@gmail.com>
2019-11-05 10:28:30 -0500

    update release version

--- plugin.php
+++ plugin.php
@@ -5,7 +5,7 @@
  * Description: Custom Gutenberg blocks includes expander card, info card, and info card set
  * Author: Andrea Hernandez Guzman
  * Author URI: https://andreahg.me/
- * Version: 1.1.0
+ * Version: 1.1.1
  * License: GPL2+
  * License URI: https://www.gnu.org/licenses/gpl-2.0.txt
  *
